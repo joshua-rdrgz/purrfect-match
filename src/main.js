79316f78c886ae3d2cfbe891739614fdb988839b
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-02-25 21:38:55 -0600

    Set up findBreed.html with JS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { createCatOptionsForm, getAvailableCats } from './pages/adoptACat';
 import { sendSMS } from './apis/twilioAPI.js';
 
-const typesOfCatsFormElement = document.getElementById('catOptions');
-const typesofCatsSubmitButton = document.getElementById('catOptionsSubmit');
-
 // ONLY WORKS ON adoptACat.html PAGE!
 if (window.location.href.split('/').at(-1) === 'adoptACat.html') {
+  const typesOfCatsFormElement = document.getElementById('catOptions');
+  const typesofCatsSubmitButton = document.getElementById('catOptionsSubmit');
   const formElements = await createCatOptionsForm(typesOfCatsFormElement);
 
   typesofCatsSubmitButton.addEventListener('click', async (e) => {
@@ -13,7 +12,16 @@ if (window.location.href.split('/').at(-1) === 'adoptACat.html') {
     await getAvailableCats(formElements);
   });
 }
+console.log('testing');
 
+if (window.location.href.split('/').at(-1) === 'findBreed.html') {
+  const submitButton = document.getElementById('imageButtonSubmit');
+  console.log(submitButton);
 
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('hello from main.js');
+  });
+}
 
 // sendSMS('Testing from main.js').then((res) => console.log(res));
