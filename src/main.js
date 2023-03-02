8151da4622703ad865969f20862c2fdd3ffba1a5
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-03-01 23:23:42 -0600

    Add subtitle to Cat Breed Result.

--- src/main.js
+++ src/main.js
@@ -6,11 +6,8 @@ import {
 import {
   getFormForUploadingImage,
   postImageToServer,
-  getRoboFlowPrediction
+  getRoboFlowPrediction,
 } from './apis/RoboflowApi';
-import {
-  sendSMS
-} from './apis/twilioAPI.js';
 
 // ONLY WORKS ON adoptACat.html PAGE!
 if (window.location.href.split('/').at(-1) === 'adoptACat.html') {
@@ -28,23 +25,40 @@ if (window.location.href.split('/').at(-1) === 'adoptACat.html') {
   });
 }
 
+// ONLY WORKS ON findBreed.html PAGE!
 if (window.location.href.split('/').at(-1) === 'findBreed.html') {
+  // GET IMAGE FORM ONTO PAGE
   const imageFormSection = document.getElementById('image-submission-form');
   const imageFormHtml = await getFormForUploadingImage();
   imageFormSection.insertAdjacentHTML('afterbegin', imageFormHtml);
 
+  // SEND IMAGE TO THE SERVER ON SUBMIT
   const imageForm = document.getElementById('image-to-server-form');
   const imageInput = document.getElementById('image-to-server-input');
   imageForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const res = await postImageToServer(imageInput.files[0]);
-    const roboFlowPredictions = await getRoboFlowPrediction(res.imageName);
-    console.log('roboFlowPredictions:', roboFlowPredictions);
-    console.log(roboFlowPredictions.data.top)
-    console.log(roboFlowPredictions.data.top.replace(/\s+/g, '-'))
-    document.getElementById('catOutputText').innerHTML = roboFlowPredictions.data.top
-    document.getElementById('catPicUnsplash').innerHTML = `https://source.unsplash.com/720x600/?${roboFlowPredictions.data.top.replace(/\s+/g, '-')}`;
+    const postImageRes = await postImageToServer(imageInput.files[0]);
+    const roboFlowPredictions = await getRoboFlowPrediction(
+      postImageRes.imageName
+    );
+    const titleElement = document.getElementById('catOutputText');
+    const subtitleElement = document.createElement('span');
+    subtitleElement.classList.add(
+      'title-font',
+      'sm:text-3xl',
+      'text-2xl',
+      'mb-3',
+      'font-small',
+      'text-white'
+    );
+    subtitleElement.innerHTML = `Your Cat's breed is:`;
+    titleElement.insertAdjacentElement('beforebegin', subtitleElement);
+    titleElement.innerHTML = roboFlowPredictions.data.top;
+    document.getElementById(
+      'catPicUnsplash'
+    ).innerHTML = `https://source.unsplash.com/720x600/?${roboFlowPredictions.data.top.replace(
+      /\s+/g,
+      '-'
+    )}`;
   });
 }
-
-// sendSMS('Testing from main.js').then((res) => console.log(res));
