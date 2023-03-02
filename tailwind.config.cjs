c6a5cf2e3e03f6d0c153782a6e12f618967dffbd
Shaurya50211 <shaurya50211@gmail.com>
2023-03-01 19:16:10 -0500

    added everything in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}', './html/*.html'],
+  content: ['./*.{html,js}', './html/*.html', '*', "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
