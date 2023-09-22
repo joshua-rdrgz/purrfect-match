a9c98a1ef7c5791e0c58c6ca2deb45a47cffb2a6
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-21 23:46:35 -0500

    remove top-level await for .then()

--- src/main.js
+++ src/main.js
@@ -17,20 +17,20 @@ if (window.location.href.split('/').at(-1) === 'adoptACat.html') {
   const displayAvailableCatsEl = document.getElementById(
     'available-cats-results'
   );
-  const formElements = await createCatOptionsForm(typesOfCatsFormElement);
+  createCatOptionsForm(typesOfCatsFormElement).then((formElements) => {
+    loadingCatsFormElement.remove();
 
-  loadingCatsFormElement.remove();
-
-  typesofCatsSubmitButton.addEventListener('click', async (e) => {
-    e.preventDefault();
-    const loadingEl = document.createElement('div');
-    loadingEl.classList.add('text-center');
-    loadingEl.textContent = 'Loading....';
-    displayAvailableCatsEl.insertAdjacentElement('beforebegin', loadingEl);
-    displayAvailableCatsEl.innerHTML = '';
-    const availableCatData = await getAvailableCats(formElements);
-    displayAvailableCats(availableCatData, displayAvailableCatsEl);
-    loadingEl.remove();
+    typesofCatsSubmitButton.addEventListener('click', async (e) => {
+      e.preventDefault();
+      const loadingEl = document.createElement('div');
+      loadingEl.classList.add('text-center');
+      loadingEl.textContent = 'Loading....';
+      displayAvailableCatsEl.insertAdjacentElement('beforebegin', loadingEl);
+      displayAvailableCatsEl.innerHTML = '';
+      const availableCatData = await getAvailableCats(formElements);
+      displayAvailableCats(availableCatData, displayAvailableCatsEl);
+      loadingEl.remove();
+    });
   });
 }
 
@@ -38,36 +38,37 @@ if (window.location.href.split('/').at(-1) === 'adoptACat.html') {
 if (window.location.href.split('/').at(-1) === 'findBreed.html') {
   // GET IMAGE FORM ONTO PAGE
   const imageFormSection = document.getElementById('image-submission-form');
-  const imageFormHtml = await getFormForUploadingImage();
-  imageFormSection.insertAdjacentHTML('afterbegin', imageFormHtml);
+  getFormForUploadingImage().then((imageFormHtml) => {
+    imageFormSection.insertAdjacentHTML('afterbegin', imageFormHtml);
 
-  // SEND IMAGE TO THE SERVER ON SUBMIT
-  const imageForm = document.getElementById('image-to-server-form');
-  const imageInput = document.getElementById('image-to-server-input');
-  imageForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const postImageRes = await postImageToServer(imageInput.files[0]);
-    const roboFlowPredictions = await getRoboFlowPrediction(
-      postImageRes.imageName
-    );
-    const titleElement = document.getElementById('catOutputText');
-    const subtitleElement = document.createElement('span');
-    subtitleElement.classList.add(
-      'title-font',
-      'sm:text-3xl',
-      'text-2xl',
-      'mb-3',
-      'font-small',
-      'text-white'
-    );
-    subtitleElement.innerHTML = `Your Cat's breed is:`;
-    titleElement.insertAdjacentElement('beforebegin', subtitleElement);
-    titleElement.innerHTML = roboFlowPredictions.data.top;
-    document.getElementById(
-      'catPicUnsplash'
-    ).innerHTML = `https://source.unsplash.com/720x600/?${roboFlowPredictions.data.top.replace(
-      /\s+/g,
-      '-'
-    )}`;
+    // SEND IMAGE TO THE SERVER ON SUBMIT
+    const imageForm = document.getElementById('image-to-server-form');
+    const imageInput = document.getElementById('image-to-server-input');
+    imageForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const postImageRes = await postImageToServer(imageInput.files[0]);
+      const roboFlowPredictions = await getRoboFlowPrediction(
+        postImageRes.imageName
+      );
+      const titleElement = document.getElementById('catOutputText');
+      const subtitleElement = document.createElement('span');
+      subtitleElement.classList.add(
+        'title-font',
+        'sm:text-3xl',
+        'text-2xl',
+        'mb-3',
+        'font-small',
+        'text-white'
+      );
+      subtitleElement.innerHTML = `Your Cat's breed is:`;
+      titleElement.insertAdjacentElement('beforebegin', subtitleElement);
+      titleElement.innerHTML = roboFlowPredictions.data.top;
+      document.getElementById(
+        'catPicUnsplash'
+      ).innerHTML = `https://source.unsplash.com/720x600/?${roboFlowPredictions.data.top.replace(
+        /\s+/g,
+        '-'
+      )}`;
+    });
   });
 }
